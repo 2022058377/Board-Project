43d3c9e15a5610ba9fd615d5cfb2a7b77e7688ff
JAEWON <bjo3963@naver.com>
2025-09-16 03:30:03 +0900

    20250916 - 좋아요 기능 구현

--- src/main/java/com/example/board/repository/LikeEntityRepository.java
+++ src/main/java/com/example/board/repository/LikeEntityRepository.java
@@ -2,7 +2,6 @@ package com.example.board.repository;
 
 import com.example.board.model.entity.LikeEntity;
 import com.example.board.model.entity.PostEntity;
-import com.example.board.model.entity.ReplyEntity;
 import com.example.board.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
